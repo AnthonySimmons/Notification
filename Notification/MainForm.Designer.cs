949dfb273dff131da731d7c11ded6f1104329057
AnthonySimmons <AnthonySimmons99@gmail.com>
2014-12-20 19:20:39 -0800

    Add CPU / RAM usage

---
+++ Notification/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Notification
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxClock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxClock
+            // 
+            this.pictureBoxClock.Location = new System.Drawing.Point(12, 60);
+            this.pictureBoxClock.Name = "pictureBoxClock";
+            this.pictureBoxClock.Size = new System.Drawing.Size(175, 172);
+            this.pictureBoxClock.TabIndex = 0;
+            this.pictureBoxClock.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBoxClock);
+            this.Name = "MainForm";
+            this.Text = "Notifications";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxClock;
+    }
+}
+
---
+++ Notification/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Notification
+{
+    public partial class MainForm : Form
+    {
+        BinaryClock binaryClock = new BinaryClock();
+
+        Timer timer;
+
+        const int ClockCellSize = 25;
+
+        const int ClockCellBuffer = 5;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void UpdateClockDisplay()
+        {
+            using (Graphics g = pictureBoxClock.CreateGraphics())
+            {
+                g.Clear(pictureBoxClock.BackColor);
+
+                int x = 0;
+
+                foreach (var column in binaryClock.TimeColumns)
+                {
+                    int y = 0;
+                    foreach (var cell in column.Cells)
+                    {
+                        Brush brush;
+                        if (cell.IsOn)
+                        {
+                            brush = Brushes.Blue;   
+                        }
+                        else
+                        {
+                            brush = Brushes.White;
+                        }
+
+                        g.FillRectangle(brush, x * (ClockCellSize + ClockCellBuffer), pictureBoxClock.Height - (y+1) * (ClockCellSize + ClockCellBuffer),
+                                ClockCellSize, ClockCellSize);
+                        y++;
+                    }
+                    x++;
+                }
+            }
+        }
+
+
+        #region Event Handlers
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            binaryClock.UpdateTime();
+            UpdateClockDisplay();
+        }
+
+        #endregion Event Handlers
+
+
+    }
+}
